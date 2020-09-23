47f8d46aeeb8a77fb27abf251ef2feab23d661a7
Harshanan Saddanathan <hsaddanathan21@gmail.com>
2020-09-23 00:39:45 -0400

    Added a few variables into script.js. Created psuedocode.md as a stream of conscience to notate process

--- script.js
+++ script.js
@@ -12,3 +12,48 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// Psuedocode Stream of Conscience
+
+1. When user pushes the generate password button: 
+    2. The character quantity prompt will appear.
+        2a. If the characters are below defined minimum
+              alert the minimum character requirements
+              prompt character quantity
+        2b else if password larger than maximum character limit
+              alert the maximum character limit
+              prompt character quantity
+    3. 
+
+
+
+
+// Variables
+var minPasswordChar = 8;
+var maxPasswordChar = 128;
+var characterConfirmArray = []
+
+// Function Declaration
+function characterConfirm(arr){
+  for (var i=0; i<arr.length;i++);{
+    confirm(arr[i]);
+    console.log(arr[i]);
+  }
+}
+
+///////Prompts //////
+
+var passwordCharacterQty = prompt("How many characters would you like your password to be?"); 
+//////Conditionals
+
+if(passwordCharacterQty.length<minPasswordChar){
+    alert("Your password must be at least " + minPasswordChar + " characters!");
+    prompt("How many characters would you like your password to be?");
+}
+else if (passwordCharacterQty>maxPasswordChar){
+  alert("Your password can't be longer than " + maxPasswordChar + " characters");
+  prompt("How many characters would you like your password to be?");
+}
+
+var allowUpperCase = confirm("Click OK to confirm "+ characterConfirmArray[i] )
